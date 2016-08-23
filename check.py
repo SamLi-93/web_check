323e0785186423e072c416378bcdfd601df78cbf
SamLi-93 <342447974@qq.com>
2016-08-23 17:03:29 +0800

    fix

--- check.py
+++ check.py
@@ -8,24 +8,6 @@ import smtplib
 import hashlib
 
 
-def _format_addr(s):
-    name, addr = parseaddr(s)
-    return formataddr(( \
-        Header(name, 'utf-8').encode(), \
-        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
-
-
-from_addr = 'mclaren1234@163.com'
-password = 'ly19940306'
-to_addr = '342447974@qq.com'
-smtp_server = 'smtp.163.com'
-
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
-msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
-
-
 def getHash(f):
     line = f.readline()
     hash = hashlib.md5()
@@ -46,32 +28,25 @@ if __name__ == '__main__':
     f2 = open("./test1.json", "rb")
 
     if (IsHashEqual(f1, f2) is False):
-        _format_addr()
+        def _format_addr(s):
+            name, addr = parseaddr(s)
+            return formataddr(( \
+                Header(name, 'utf-8').encode(), \
+                addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+
+        from_addr = 'mclaren1234@163.com'
+        password = 'ly19940306'
+        to_addr = '342447974@qq.com'
+        smtp_server = 'smtp.163.com'
+
+        msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+        msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+        msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+        msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
         server = smtplib.SMTP(smtp_server, 25)
         server.set_debuglevel(1)
         server.login(from_addr, password)
         server.sendmail(from_addr, [to_addr], msg.as_string())
         server.quit()
-
-
-        # def _format_addr(s):
-        #     name, addr = parseaddr(s)
-        #     return formataddr(( \
-        #         Header(name, 'utf-8').encode(), \
-        #         addr.encode('utf-8') if isinstance(addr, unicode) else addr))
-        #
-        # from_addr = 'mclaren1234@163.com'
-        # password = 'ly19940306'
-        # to_addr = '342447974@qq.com'
-        # smtp_server = 'smtp.163.com'
-        #
-        # msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-        # msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
-        # msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-        # msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
-
-        # server = smtplib.SMTP(smtp_server, 25)
-        # server.set_debuglevel(1)
-        # server.login(from_addr, password)
-        # server.sendmail(from_addr, [to_addr], msg.as_string())
-        # server.quit()
